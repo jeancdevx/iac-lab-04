4f78bb39573cc852e0856f210ef119b89a1d98fc
jeancdev <jcode2006@gmail.com>
2026-05-04 00:25:04 -0500

    Add S3 bucket policy to deny insecure transport

---
+++ iac/modules/s3/policy.tf
@@ -0,0 +1,28 @@
+data "aws_iam_policy_document" "bucket" {
+  statement {
+    sid     = "DenyInsecureTransport"
+    effect  = "Deny"
+    actions = ["s3:*"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.bucket.json
+}
