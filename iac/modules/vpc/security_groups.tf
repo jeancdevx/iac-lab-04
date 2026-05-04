6ade320d8ed536d42fb2f62070eb329ecd6b5b0a
jeancdev <jcode2006@gmail.com>
2026-05-04 00:25:11 -0500

    Remove unnecessary egress rule for SQS interface from upload Lambda security group

--- iac/modules/vpc/security_groups.tf
+++ iac/modules/vpc/security_groups.tf
@@ -8,14 +8,6 @@ resource "aws_security_group" "lambda_upload" {
   vpc_id      = aws_vpc.this.id
 
   egress {
-    description = "HTTPS to the SQS interface endpoint"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.vpc_cidr]
-  }
-
-  egress {
     description     = "HTTPS to the S3 gateway endpoint"
     from_port       = 443
     to_port         = 443
